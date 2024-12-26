f34c4ec7dafa5f3089e9149c4c0944e9f0cddb6e
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2024-12-26 14:18:48 -0300

    example of bootstrap by module included

---
+++ application/modules/Main/Bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Application\Main;
+
+/***
+ * class to bootstrap the module
+ */
+class Bootstrap {
+
+	/**
+	 * any method that starts with "init" will be called by the bootstrap
+	 */
+	public function initDatabase()
+	{
+		// create databases
+	}
+
+	public function initSessions()
+	{
+		// verify authentication and another stuffs
+	}
+}
